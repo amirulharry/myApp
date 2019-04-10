2a41301e0b89b3bad9962040076c2229a13425ae
[azmi353665] <[mohdamirul.azreen@exact.com]>
2019-04-10 20:11:34 +0800

    second commit

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import List from './List.js'
+import firebase from 'react-native-firebase'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,6 +20,50 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+   componentDidMount() {
+    var config = {
+      apiKey: "AIzaSyC093mcKJ8ilcbgOODnHk5Hyr72urQdTyA",
+      authDomain: "exact-hackathon.firebaseapp.com",
+      databaseURL: "https://exact-hackathon.firebaseio.com",
+      projectId: "exact-hackathon",
+      storageBucket: "exact-hackathon.appspot.com",
+      messagingSenderId: "539869004879",
+    };
+    firebase.initializeApp(config);
+
+    console.log('Component did mount2');
+
+          firebase.messaging().requestPermission()
+            .then(() => {
+              console.log('Permission completed');
+            });
+
+          setTimeout(() => {
+            firebase.messaging().getToken()
+              .then(token => {
+
+                firebase.database().ref("Token").push({
+                  token: token
+                }).then((data) => {
+
+                  alert("save:" + token);
+                }).catch((error) => {
+                  //error callback
+                  alert("can't save " + error);
+                })
+
+                this.setState({ token });
+                alert(token);
+              })
+              .catch(err => {
+                alert(err);
+              });
+          }, 3000);
+
+    this.doThings;
+  }
+  
   render() {
     return (
       
